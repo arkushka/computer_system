f8c8a622649f0b223e49dec922df0a1cadb4602c
Yulia <32716680+arkushka@users.noreply.github.com>
2018-03-27 15:20:45 +0300

    Program.cs of first lab

---
+++ Lab#1/Base64.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Base64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string base64alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+            List<byte> bytes = new List<byte>();
+            int c;
+            string end = "";
+            using (BinaryReader br = new BinaryReader(new FileStream(args[0], FileMode.Open)))
+            {
+                for (long i = 0; i < br.BaseStream.Length; i++)
+                    bytes.Add(br.ReadByte());
+            }
+            c = bytes.Count % 3;
+            if (c > 0)
+                for (; c < 3; c++)
+                {
+                    bytes.Add(0);
+                    end+= "=";
+                }
+
+            string foo = "";
+
+            for (int i = 0; i < bytes.Count; i = i + 3)
+            {
+                foo += base64alphabet[bytes[i] >> 2];
+                int secondbyte = ((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4);
+                foo += base64alphabet[secondbyte];
+                int thirdbyte = ((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6);
+                foo += base64alphabet[thirdbyte];
+                foo += base64alphabet[bytes[i + 2] & 63];
+            }
+            foo = foo.Substring(0, foo.Length - end.Length) + end;
+            using (StreamWriter swrtr = new StreamWriter(new FileStream(args[1], FileMode.Create)))
+            {
+                swrtr.Write(foo);
+            }
+        }
+    }
+}
---
+++ Lab#1/Info.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, double> frequencies = new Dictionary<char, double>();
+            int charCount = 0;
+            using (StreamReader sr = new StreamReader(new FileStream(args[0], FileMode.Open)))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    charCount += line.Length;
+                    for (int i = 0; i < line.Length; ++i)
+                    {
+                        double val;
+                        if (frequencies.TryGetValue(line[i], out val))
+                        {
+                            frequencies[line[i]] = val + 1.0;
+                        }
+                        else
+                        {
+                            frequencies.Add(line[i], 1.0);
+                        }
+                    }
+                }
+            }
+            List<char> keys = new List<char>(frequencies.Keys);
+            for (int i = 0; i < keys.Count; ++i)
+            {
+                frequencies[keys[i]] /= charCount;
+            }
+
+            double ent = 0.0;
+            foreach (char key in frequencies.Keys)
+            {
+                ent += frequencies[key] * Math.Log(frequencies[key], 2.0);
+            }
+            ent *= -1;
+
+            double infoAmount = ent * charCount/8;
+
+            foreach (char key in frequencies.Keys)
+            {
+                Console.WriteLine(key + " - " + frequencies[key]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Enthropy: " + ent);
+            Console.WriteLine("Info amount: " + infoAmount);
+            Console.WriteLine("File size: " + new FileInfo(args[0]).Length);
+        }
+    }
+}
